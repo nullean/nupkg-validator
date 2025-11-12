c1b2cc044d135d94210fb33f054a6bf9772ef3fd
Martijn Laarman <Mpdreamz@gmail.com>
2025-11-12 15:57:12 +0100

    Update minver and bullseye (#17)

--- build/scripts/Targets.fs
+++ build/scripts/Targets.fs
@@ -14,17 +14,15 @@ let exec binary args =
 let private restoreTools = lazy (exec "dotnet" [ "tool"; "restore" ])
 
 let private currentVersion =
-    lazy (
-         restoreTools.Value |> ignore
-         let r = Proc.Start("dotnet", "minver", "-p", "canary.0")
-         let o = r.ConsoleOut |> Seq.find (fun l -> not (l.Line.StartsWith("MinVer:")))
-         o.Line
+    lazy(
+        restoreTools.Value |> ignore
+        let r = Proc.Start("dotnet", "minver", "-p=canary.0", "-v=e", "-m=0.1")
+        let o = r.ConsoleOut |> Seq.find (fun l -> not(l.Line.StartsWith("MinVer:")))
+        o.Line
     )
 
 let private currentVersionInformational =
-    lazy (
-        sprintf "%s+%s" currentVersion.Value (Information.getCurrentSHA1 ".")
-    )
+    lazy (sprintf "%s+%s" currentVersion.Value (Information.getCurrentSHA1 "."))
 
 let private clean (arguments: ParseResults<Arguments>) =
     if Paths.Output.Exists then
